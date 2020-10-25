575321b15dc3b4cf8407a9ed2b61b6603b4c16de
tommyrharper <thomasrobertharper@gmail.com>
2020-10-25 20:50:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-const contents = document.getElementById('contents');
-contents.parentNode.removeChild(contents);
+// Pure JS
+// const contents = document.getElementById('contents');
+// contents.parentNode.removeChild(contents);
+
+// jQuery
+var contents = $('#contents');
+var contentsParent = contents.parent();
+contents.remove;
+contentsParnet.prepend('https://i.redd.it/czndqfla5fh21.jpg');
